b9057b7af542e80086ba29e090240ac6bd962d12
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-13 21:54:04 -0300

    :art: update Text.stories.tsx

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -9,4 +9,16 @@ export default {
   },
 } as Meta<TextProps>;
 
-export const Default: StoryObj = {}
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm',
+  }
+}
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg',
+  }
+}
