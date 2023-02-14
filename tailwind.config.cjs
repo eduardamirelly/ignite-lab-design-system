8daf877b2c48aac9e232d878d905cd602864a25d
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-13 21:24:24 -0300

    :lipstick: set colors on tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,19 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    colors: {
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7C7C8A',
+        100: '#E1E1E6',
+        200: '#C4C4CC',
+      },
+      cyan: {
+        500: '#81D8F7',
+        300: '#98E1FB',
+      },
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif',
