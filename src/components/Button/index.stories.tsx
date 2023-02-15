c8bc9e9712c973ef4d80c5cc9e323f3669c3c857
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-15 20:51:59 -0300

    :lipstick: styling Button and update Button stories

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -4,6 +4,16 @@ import { Button, ButtonProps } from ".";
 export default {
   title: 'Components/Button',
   component: Button,
+  args: {
+    children: 'Button',
+  },
+  argTypes: {
+    asChild: {
+      table: {
+        disable: true,
+      }
+    }
+  }
 } as Meta<ButtonProps>;
 
 export const Default: StoryObj<ButtonProps> = {}
