20f744bb99831190e0a4cd39a4ac10b00ca4a8d8
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-14 22:18:33 -0300

    :art: update Heading stories

--- src/components/Heading/index.stories.tsx
+++ src/components/Heading/index.stories.tsx
@@ -1,7 +1,9 @@
-import { Meta } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import { Heading } from ".";
 
 export default {
   title: 'Components/Heading',
   component: Heading,
 } as Meta;
+
+export const Default: StoryObj = {}
