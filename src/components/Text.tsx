c4dde76095d2ce407d8faa63c8f0d5d769b361c6
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-14 22:02:53 -0300

    :art: update Text.tsx

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,13 +1,17 @@
+import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
 
 export interface TextProps {
   size?: 'sm' | 'md' | 'lg';
   children: string;
+  asChild?: boolean;
 }
 
-export function Text({ size = 'md', children }: TextProps) {
+export function Text({ size = 'md', children, asChild }: TextProps) {
+  const Comp = asChild ? Slot : 'span';
+
   return (
-    <span
+    <Comp
       className={
         clsx('text-gray-100 font-sans',
         {
@@ -18,6 +22,6 @@ export function Text({ size = 'md', children }: TextProps) {
       )}
     >
       {children}
-    </span>
+    </Comp>
   )
 }
