921f91befa0975a652b9068afbca14669ea23305
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-14 22:17:03 -0300

    :art: create Heading component

---
+++ src/components/Heading/index.stories.tsx
@@ -0,0 +1,7 @@
+import { Meta } from "@storybook/react";
+import { Heading } from ".";
+
+export default {
+  title: 'Components/Heading',
+  component: Heading,
+} as Meta;
---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,5 @@
+export function Heading() {
+  return (
+    <h1>Heading</h1>
+  )
+}
