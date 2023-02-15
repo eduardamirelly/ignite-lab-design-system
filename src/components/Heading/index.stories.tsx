794924fd60c9ef46f81e573c582bb533671c1da1
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-15 20:50:43 -0300

    :art: update Heading stories

--- src/components/Heading/index.stories.tsx
+++ src/components/Heading/index.stories.tsx
@@ -14,6 +14,11 @@ export default {
       control: {
         type: 'inline-radio',
       }
+    },
+    asChild: {
+      table: {
+        disable: true,
+      }
     }
   }
 } as Meta<HeadingProps>;
@@ -41,11 +46,6 @@ export const CustomComponent: StoryObj<HeadingProps> = {
   },
   argTypes: {
     children: {
-      table: {
-        disable: true,
-      }
-    },
-    asChild: {
       table: {
         disable: true,
       }
