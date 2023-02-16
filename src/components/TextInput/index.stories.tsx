d62a37f58f81a5205ae8360b031f359053a66548
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-15 21:23:09 -0300

    :lipstick: create TextInput stories and component

---
+++ src/components/TextInput/index.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { TextInput, TextInputProps } from ".";
+
+export default {
+  title: 'Components/TextInput',
+  component: TextInput,
+} as Meta<TextInputProps>;
+
+export const Default: StoryObj<TextInputProps> = {}
---
+++ src/components/TextInput/index.tsx
@@ -0,0 +1,9 @@
+export interface TextInputProps {
+
+}
+
+export function TextInput() {
+  return (
+    <h1>TextInput</h1>
+  )
+}
