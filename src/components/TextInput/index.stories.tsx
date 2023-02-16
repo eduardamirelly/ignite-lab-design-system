9f5bbb4093d36f6e6c609785633e20d75f25a9ea
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-15 21:59:43 -0300

    💄 add other variation to TextInput

--- src/components/TextInput/index.stories.tsx
+++ src/components/TextInput/index.stories.tsx
@@ -23,3 +23,11 @@ export default {
 } as Meta<TextInputRootProps>;
 
 export const Default: StoryObj<TextInputRootProps> = {}
+
+export const WithoutIcon: StoryObj<TextInputRootProps> = {
+  args: {
+    children: (
+      <TextInput.Input placeholder="Placeholder" />
+    )
+  }
+}
