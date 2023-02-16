95dd02073a4d969df471af09dea6836ba1b9b27e
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-15 21:33:20 -0300

    :lipstick: basic styling on TextInput

--- src/components/TextInput/index.stories.tsx
+++ src/components/TextInput/index.stories.tsx
@@ -4,6 +4,9 @@ import { TextInput, TextInputProps } from ".";
 export default {
   title: 'Components/TextInput',
   component: TextInput,
+  args: {
+    placeholder: 'Placeholder',
+  }
 } as Meta<TextInputProps>;
 
 export const Default: StoryObj<TextInputProps> = {}
--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,9 +1,15 @@
-export interface TextInputProps {
+import { clsx } from "clsx";
+import { InputHTMLAttributes } from "react";
 
-}
+export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export function TextInput() {
+export function TextInput(props: TextInputProps) {
   return (
-    <h1>TextInput</h1>
+    <input
+      {...props}
+      className={clsx(
+        'py-4 px-3 rounded bg-gray-800 w-full outline-none text-gray-100 text-xs placeholder:text-gray-400 focus:ring-2 ring-cyan-300',
+      )}
+    />
   )
 }
