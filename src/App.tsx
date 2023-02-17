2b6c799ac1d1df20b5ac1f3cddeda8b57ad68bdf
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-16 21:41:56 -0300

    💄 header on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { Heading } from './components/Heading';
+import { Text } from './components/Text';
+import { Logo } from './Logo';
 import './styles/global.css';
 
 function App() {
   return (
-    <div>
-      Hello World!
+    <div className="w-screen h-screen bg-gray-900 flex items-center justify-center flex-col text-gray-100">
+      <Logo />
+
+      <header className="flex flex-col items-center">
+        <Heading size="lg" className="mt-4">
+          Ignite Lab
+        </Heading>
+
+        <Text size="lg" className="text-gray-400 mt-1">
+          Faça login e comece a usar!
+        </Text>
+      </header>
     </div>
   )
 }
