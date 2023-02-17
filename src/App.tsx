51b8d885368c5126870b961fc08c5653786133d9
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-16 21:57:19 -0300

    🎨 add footer

--- src/App.tsx
+++ src/App.tsx
@@ -59,6 +59,15 @@ function App() {
           Entrar na plataforma
         </Button>
       </form>
+
+      <footer className="flex flex-col items-center gap-4 mt-8">
+        <Text asChild size="sm">
+          <a href="#" className="text-gray-400 underline hover:text-gray-200">Esqueceu sua senha?</a>
+        </Text>
+        <Text asChild size="sm">
+          <a href="#" className="text-gray-400 underline hover:text-gray-200">Não possue conta? Crie uma agora!</a>
+        </Text>
+      </footer>
     </div>
   )
 }
